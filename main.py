7dc61407f34fa88a0e72647d80dd097a50467634
dairo <dcarrilloa@unal.edu.co>
2022-04-30 10:24:05 -0500

    version for presentation

--- main.py
+++ main.py
@@ -12,56 +12,28 @@ if __name__ == "__main__":
     scraper = []
     offset = 0
     with TikTokApi() as api:
-        #    tag = api.hashtag(name="petro")
-        #    print(tag.info())
+        temas = ['petro','fico']
+        for tema in temas:
+            while offset < number_video:
+                for video in api.search.videos(search_term=tema, count=number_video, offset=offset):
+                    metadata = {'tema': 'petro',
+                                'create_video': video.create_time,
+                                'video_id': video.id,
+                                'author': video.as_dict['author'],
+                                'hashtags': video.hashtags,
+                                'desc': video.as_dict['desc'],
+                                '_browser': getattr(video.parent, '_browser'),
+                                'lenguage': getattr(video.parent, '_language'),
+                                'time_zone': getattr(video.parent, '_timezone_name'),
+                                'source': getattr(video.parent, '_user_agent'),
+                                'stats': video.stats}
 
-        while offset < number_video:
-            for video in api.search.videos(search_term='petro', count=number_video, offset=offset):
-                # for video in api.hashtag(name='funny').videos():
-                metadata = {'tema': 'petro',
-                            'create_video': video.create_time,
-                            'video_id': video.id,
-                            'author': video.as_dict['author'],
-                            'hashtags': video.hashtags,
-                            'desc': video.as_dict['desc'],
-                            '_browser': getattr(video.parent, '_browser'),
-                            'lenguage': getattr(video.parent, '_language'),
-                            'time_zone': getattr(video.parent, '_timezone_name'),
-                            'source': getattr(video.parent, '_user_agent'),
-                            'stats': video.stats}
-
-                for key, value in metadata.items():
-                    if '|' in str(value):
-                        metadata[key] = str(value).replace('|', '-')
-                # video.stats
-                scraper.append(metadata)
-
-            for video in api.search.videos(search_term='fico', count=number_video, offset=offset):
-                # for video in api.hashtag(name='funny').videos():
-                metadata = {'tema': 'fico',
-                            'create_video': video.create_time,
-                            'video_id': video.id,
-                            'author': video.as_dict['author'],
-                            'hashtags': video.hashtags,
-                            'desc': video.as_dict['desc'],
-                            '_browser': getattr(video.parent, '_browser'),
-                            'lenguage': getattr(video.parent, '_language'),
-                            'time_zone': getattr(video.parent, '_timezone_name'),
-                            'source': getattr(video.parent, '_user_agent'), 'stats': video.stats}
-
-                for key, value in metadata.items():
-                    if '|' in str(value):
-                        metadata[key] = str(value).replace('|', '-')
-                # video.stats
-                scraper.append(metadata)
-            offset = offset + 400
-            time.sleep(5)
+                    for key, value in metadata.items():
+                        if '|' in str(value):
+                            metadata[key] = str(value).replace('|', '-')
+                    scraper.append(metadata)
+                offset = offset + 400
+                time.sleep(5)
 
     df = pd.DataFrame(scraper)
-    df.to_csv('data_big_data.csv', sep='|', encoding='utf-8')
-    # video_bytes = api.video(id=video.id).bytes()
-    # Saving The Video
-    # with open('saved_video.mp4', 'wb') as output:
-    #    output.write(video_bytes)
-# for video in tag.videos():
-#    print(video.id)
+    df.to_csv('data_big_data_result.csv', sep='|', encoding='utf-8')
